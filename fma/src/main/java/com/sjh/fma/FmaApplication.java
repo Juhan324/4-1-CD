8c412b761b63d39490d737027936343bacea6f50
Juhan324 <77876969+Juhan324@users.noreply.github.com>
2022-04-04 17:45:18 +0900

    Create project

---
+++ fma/src/main/java/com/sjh/fma/FmaApplication.java
@@ -0,0 +1,13 @@
+package com.sjh.fma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmaApplication.class, args);
+	}
+
+}
---
+++ fma/src/test/java/com/sjh/fma/FmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjh.fma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
