5817bb4167ff2f6cc56f3fd01217dd62ac52aa24
Juhan324 <77876969+Juhan324@users.noreply.github.com>
2022-04-06 00:46:43 +0900

    Remake project

---
+++ fma/src/main/java/com/sjh/fmt/FmtApplication.java
@@ -0,0 +1,13 @@
+package com.sjh.fmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmtApplication.class, args);
+	}
+
+}
---
+++ fma/src/test/java/com/sjh/fmt/FmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjh.fmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
